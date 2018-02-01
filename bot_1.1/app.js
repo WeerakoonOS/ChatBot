cdcc3f6233737fc8517f2bd0622bb8751b0ea851
WeerakoonOS <oswucsc@gmail.com>
2018-02-01 19:57:47 +0530

    LUIS recognizer

--- bot_1.1/app.js
+++ bot_1.1/app.js
@@ -1,5 +1,4 @@
 /* jshint esversion: 6 */
-"use strict";
 require('dotenv').config();
 const restify = require('restify');
 const fs = require('fs');
@@ -28,25 +27,63 @@ var connector = new builder.ChatConnector({
 // Listen for messages from users
 server.post('/api/messages', connector.listen());
 
-var bot = new builder.UniversalBot(connector, [
+var bot = new builder.UniversalBot(connector, (session, args, next) => {
+    session.endDialog(`I'm sorry, I did not understand '${session.message.text}'.\nType 'help' to know more about me :)`);
+});
+
+var luisRecognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL).onEnabled(function (context, callback) {
+    var enabled = context.dialogStack().length === 0;
+    callback(null, enabled);
+});
+bot.recognizer(luisRecognizer);
+
+bot.dialog('Help',
     (session, args, next) => {
-        session.sendTyping();
-        session.send('Hi! I\'m the help desk bot and I can help you create a ticket.');
-        builder.Prompts.text(session, 'First, please briefly describe your problem to me.');
-    },
-    (session, result, next) => {
-        session.dialogData.description = result.response;
+        session.endDialog(`I'm the help desk bot and I can help you create a ticket.\n` +
+            `You can tell me things like _I need to reset my password_ or _I cannot print_.`);
+        session.send('First, please briefly describe your problem to me.');
+    }
+).triggerAction({
+    matches: 'Help'
+});
 
-        var choices = ['high', 'normal', 'low'];
-        builder.Prompts.choice(session, 'Which is the severity of this problem?', choices, { listStyle: builder.ListStyle.button });
-    },
-    (session, result, next) => {
-        session.dialogData.severity = result.response.entity;
+bot.dialog('SubmitTicket', [
+    (session, args, next) => {
+        var category = builder.EntityRecognizer.findEntity(args.intent.entities, 'category');
+        var severity = builder.EntityRecognizer.findEntity(args.intent.entities, 'severity');
 
-        builder.Prompts.text(session, 'Which would be the category for this ticket (software, hardware, networking, security or other)?');
+        if (category && category.resolution.values.length > 0) {
+            session.dialogData.category = category.resolution.values[0];
+        }
+
+        if (severity && severity.resolution.values.length > 0) {
+            session.dialogData.severity = severity.resolution.values[0];
+        }
+
+        session.dialogData.description = session.message.text;
+
+        if (!session.dialogData.severity) {
+            var choices = ['high', 'normal', 'low'];
+            builder.Prompts.choice(session, 'Which is the severity of this problem?', choices, { listStyle: builder.ListStyle.button });
+        } else {
+            next();
+        }
     },
     (session, result, next) => {
-        session.dialogData.category = result.response;
+        if (!session.dialogData.severity) {
+            session.dialogData.severity = result.response.entity;
+        }
+
+        if (!session.dialogData.category) {
+            builder.Prompts.text(session, 'Which would be the category for this ticket (software, hardware, networking, security or other)?');
+        } else {
+            next();
+        }
+    },
+    (session, result, next) => {
+        if (!session.dialogData.category) {
+            session.dialogData.category = result.response;
+        }
 
         var message = `Great! I'm going to create a "${session.dialogData.severity}" severity ticket in the "${session.dialogData.category}" category. ` +
                       `The description I will use is "${session.dialogData.description}". Can you please confirm that this information is correct?`;
@@ -54,34 +91,34 @@ var bot = new builder.UniversalBot(connector, [
         builder.Prompts.confirm(session, message, { listStyle: builder.ListStyle.button });
     },
     (session, result, next) => {
-        session.sendTyping();// send 'typing' to user
         if (result.response) {
             var data = {
                 category: session.dialogData.category,
                 severity: session.dialogData.severity,
                 description: session.dialogData.description,
-            }
-    
+            };
+
             const client = restify.createJsonClient({ url: ticketSubmissionUrl });
-    
+
             client.post('/api/tickets', data, (err, request, response, ticketId) => {
                 if (err || ticketId == -1) {
-                    session.send('Something went wrong while I was saving your ticket. Please try again later.')
+                    session.send('Ooops! Something went wrong while I was saving your ticket. Please try again later.');
                 } else {
                     session.send(new builder.Message(session).addAttachment({
-                        
                         contentType: "application/vnd.microsoft.card.adaptive",
                         content: createCard(ticketId, data)
                     }));
                 }
-    
+
                 session.endDialog();
             });
         } else {
             session.endDialog('Ok. The ticket was not created. You can start again if you want.');
         }
     }
-]);
+]).triggerAction({
+    matches: 'SubmitTicket'
+});
 
 const createCard = (ticketId, data) => {
     var cardTxt = fs.readFileSync('./cards/ticket.json', 'UTF-8');
